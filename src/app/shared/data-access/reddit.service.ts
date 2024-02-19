b49360385ea08926bc19301142ae2ab36039243e
lashagelashvili <las.gelashvili@gmail.com>
2024-02-19 14:12:14 +0400

    update reddit service to fetch gifs

--- src/app/shared/data-access/reddit.service.ts
+++ src/app/shared/data-access/reddit.service.ts
@@ -1,7 +1,8 @@
-import { Injectable, computed, signal } from '@angular/core';
+import { Injectable, computed, inject, signal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { Gif } from '../interfaces';
-import { of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Gif, RedditPost, RedditResponse } from '../interfaces';
+import { EMPTY, catchError, map } from 'rxjs';
 
 export interface GifsState {
   gifs: Gif[];
@@ -9,6 +10,8 @@ export interface GifsState {
 
 @Injectable({ providedIn: 'root' })
 export class RedditService {
+  private http = inject(HttpClient);
+
   // state
   private state = signal<GifsState>({
     gifs: [],
@@ -17,18 +20,8 @@ export class RedditService {
   // selectors
   gifs = computed(() => this.state().gifs);
 
-  // sources
-  gifsLoaded$ = of([
-    {
-      src: '',
-      author: '',
-      name: '',
-      permalink: '',
-      title: 'test gif',
-      thumbnail: '',
-      comments: 0,
-    },
-  ]);
+  //sources
+  private gifsLoaded$ = this.fetchFromReddit('gifs');
 
   constructor() {
     //reducers
@@ -38,5 +31,72 @@ export class RedditService {
         gifs: [...state.gifs, ...gifs],
       }))
     );
+  }
+
+  private fetchFromReddit(subreddit: string) {
+    return this.http
+      .get<RedditResponse>(
+        `https://www.reddit.com/r/${subreddit}/hot/.json?limit=100`
+      )
+      .pipe(
+        catchError((err) => EMPTY),
+        map((response) => this.convertRedditPostsToGifs(response.data.children))
+      );
+  }
+
+  private convertRedditPostsToGifs(posts: RedditPost[]) {
+    const defaultThumbnails = ['default', 'none', 'nsfw'];
+
+    return posts
+      .map((post) => {
+        const thumbnail = post.data.thumbnail;
+        const modifiedThumbnail = defaultThumbnails.includes(thumbnail)
+          ? `/assets/${thumbnail}.png`
+          : thumbnail;
+
+        return {
+          src: this.getBestSrcForGif(post),
+          author: post.data.author,
+          name: post.data.name,
+          permalink: post.data.permalink,
+          title: post.data.title,
+          thumbnail: modifiedThumbnail,
+          comments: post.data.num_comments,
+        };
+      })
+      .filter((post): post is Gif => post.src !== null);
+  }
+
+  private getBestSrcForGif(post: RedditPost) {
+    // If the source is in .mp4 format, leave unchanged
+    if (post.data.url.indexOf('.mp4') > -1) {
+      return post.data.url;
+    }
+
+    // If the source is in .gifv or .webm formats, convert to .mp4 and return
+    if (post.data.url.indexOf('.gifv') > -1) {
+      return post.data.url.replace('.gifv', '.mp4');
+    }
+
+    if (post.data.url.indexOf('.webm') > -1) {
+      return post.data.url.replace('.webm', '.mp4');
+    }
+
+    // If the URL is not .gifv or .webm, check if media or secure media is available
+    if (post.data.secure_media?.reddit_video) {
+      return post.data.secure_media.reddit_video.fallback_url;
+    }
+
+    if (post.data.media?.reddit_video) {
+      return post.data.media.reddit_video.fallback_url;
+    }
+
+    // If media objects are not available, check if a preview is available
+    if (post.data.preview?.reddit_video_preview) {
+      return post.data.preview.reddit_video_preview.fallback_url;
+    }
+
+    // No useable formats available
+    return null;
   }
 }
